4a9d09ea4c3554246e6507fe1c736fb1999d56c8
Justin Taylor <s.justin.taylor@gmail.com>
2020-03-24 21:00:49 +1000

    added private fields, getters to BillboardModel

--- src/Server/Controllers/BillboardController.java
+++ src/Server/Controllers/BillboardController.java
@@ -10,19 +10,13 @@ public class BillboardController {
         this.model = model;
     }
     public String getBillboard(){
-        // Call this.model.getBillboard() to get a ResultSet.
-        // Build a Document object with the resultset: https://mkyong.com/java/how-to-create-xml-file-in-java-dom/
+        // Call this.model.getBillboard() to populate the object.
+        // Use the getter methods to build a Document object  https://mkyong.com/java/how-to-create-xml-file-in-java-dom/
         // Convert the Document to a string (gonna need to google it. Not complicated though)
         // Return the stringified xml.
         // Don't forget error handling (try catch, exceptions etc)
-        ResultSet rs = this.model.getBillboard();
-        int res = 0;
-        try{
-        while(rs.next()){
-            res +=rs.getInt("Column1");
-        }}catch(Exception e){
-            System.out.println(e.getMessage());
-        }
+        this.model.getBillboard();
+
         return "<billboard>xml</billboard>" + res;
     }
 
--- src/Server/Models/BillboardModel.java
+++ src/Server/Models/BillboardModel.java
@@ -9,16 +9,82 @@ import java.sql.ResultSet;
 * */
 public class BillboardModel {
     private Database dbConn;
+    private int id;
+    private String background;
+    private String message;
+    private String message_color;
+    private String url;
+    private String data;
+    private String information;
+    private String information_color;
+    private String start_time;
+    private String end_time;
     public BillboardModel(Database dbConnection){
         // Gets passed the database connection on creation, which is then used to retrieve and set billboard specific
         // records.
         this.dbConn = dbConnection;
+        this.background = "";
+        this.id = 0;
+        this.message = "";
+        this.message_color = "";
+        this.url = "";
+        this.data = "";
+        this.information = "";
+        this.information_color = "";
+        this.start_time = "";
+        this.end_time = "";
     }
-    public ResultSet getBillboard(){
+    public int getId(){
+        return this.id;
+    }
+    public String getBackground(){
+        return this.background;
+    }
+    public String getMessage(){
+        return this.message;
+    }
+    public String getMessage_color(){
+        return this.message_color;
+    }
+    public String getUrl(){
+        return this.url;
+    }
+    public String getData(){
+        return this.data;
+    }
+    public String getInformation(){
+        return this.information;
+    }
+    public String getInformation_color(){
+        return this.information_color;
+    }
+    public String getStart_time(){
+        return this.start_time;
+    }
+    public String getEnd_time(){
+        return this.end_time;
+    }
+    public void getBillboard(){
         // Returns a ResultSet containing the results of the query
-        return this.dbConn.runQuery("select * from test");
+        ResultSet rs = this.dbConn.runQuery("select * from test");
+        try{
+            while(rs.next()){
+                this.id = rs.getInt("id");
+                this.background = rs.getString("background");
+                this.message = rs.getString("message");
+                this.message_color = rs.getString("message_color");
+                this.url = rs.getString("url");
+                this.data = rs.getString("data");
+                this.information = rs.getString("information");
+                this.information_color = rs.getString("information_color");
+                this.start_time = rs.getString("start_time");
+                this.end_time = rs.getString("end_time");
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
-    public ResultSet deleteBillboard(int billboardID){
-        return this.dbConn.runQuery("delete * from test where Column1 = 1");
+    public void deleteBillboard(int billboardID){
+        this.dbConn.runQuery("delete * from test where Column1 = 1");
     }
 }
